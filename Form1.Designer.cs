5911a2ab70fef667438902221bcb578b76187ec1
MianC <MianC@D9T327T3>
2023-03-22 16:21:27 +0000

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace tSwitch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbDatabases = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSQLServer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lbDatabases
+            // 
+            this.lbDatabases.FormattingEnabled = true;
+            this.lbDatabases.Location = new System.Drawing.Point(19, 44);
+            this.lbDatabases.Name = "lbDatabases";
+            this.lbDatabases.Size = new System.Drawing.Size(267, 134);
+            this.lbDatabases.TabIndex = 0;
+            this.lbDatabases.DoubleClick += new System.EventHandler(this.lbDatabases_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SQL Server";
+            // 
+            // txtSQLServer
+            // 
+            this.txtSQLServer.Enabled = false;
+            this.txtSQLServer.Location = new System.Drawing.Point(87, 16);
+            this.txtSQLServer.Name = "txtSQLServer";
+            this.txtSQLServer.Size = new System.Drawing.Size(199, 20);
+            this.txtSQLServer.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 199);
+            this.Controls.Add(this.txtSQLServer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbDatabases);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "tSwitch - TSV DB Switch";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbDatabases;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSQLServer;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace tSwitch
+{
+    public partial class Form1 : Form
+    {
+        private string _connectionStringXpath = Properties.Settings.Default.TSVConfigXPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSQLServer.Text = $"\\\\{Properties.Settings.Default.SQLDataSource}";
+            lbDatabases.DataSource = DatabaseList();
+        }
+
+        private List<string> DatabaseList()
+        {
+
+            List<string> databaseList = new List<string>();
+            string sqlDataSource = Properties.Settings.Default.SQLDataSource;
+            string sqlPassword = Properties.Settings.Default.SQLPassword;
+
+            string connectionString = $"Data Source={sqlDataSource};Initial Catalog=master;User ID=sa;Password={sqlPassword};Connection Timeout=300";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT name FROM sys.databases where name like 'TSV%' ", connection);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    databaseList.Add(reader[0].ToString());
+                }
+            }
+            return databaseList;
+        
+        }
+
+        private string getCurrentDatabase()
+        {
+            // Load the xml file
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Properties.Settings.Default.TSVConfig);
+
+            //get the "add" element wiht key = "ConnectionString"
+            XmlNode xmlNode = xmlDoc.SelectSingleNode(_connectionStringXpath);
+
+            string currentString = xmlNode.Attributes["value"].Value;
+
+            foreach (string s in currentString.Split(';'))
+            {
+                if (s.IndexOf("Catalog") > 0)
+                    return s.Split('=')[1].ToString();
+            }
+            return "";
+        }
+
+        private void lbDatabases_DoubleClick(object sender, EventArgs e)
+        {
+            string selectedDatabase = lbDatabases.SelectedValue.ToString();
+            string currentDatabase = getCurrentDatabase();
+            if (MessageBox.Show($"Switch TSV database [{currentDatabase}] to [{selectedDatabase}]?", "Swtich TSV database", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+               UpdateTSVConfig(currentDatabase, selectedDatabase);           
+        }
+
+        private void UpdateTSVConfig(string currentDb, string selectedDb)
+        {
+            // Load the xml file
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(Properties.Settings.Default.TSVConfig);
+
+            //get the "add" element wiht key = "ConnectionString"
+            XmlNode xmlNode = xmlDoc.SelectSingleNode(_connectionStringXpath);
+
+            string currentString = xmlNode.Attributes["value"].Value;
+            string newString = currentString.Replace(currentDb, selectedDb);
+            xmlNode.Attributes["value"].Value = newString;
+            try
+            {
+                xmlDoc.Save(Properties.Settings.Default.TSVConfig);
+                MessageBox.Show($"Current database is set to: [{selectedDb}]. Please restart TSV", "TSV DB switched successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occured while updating the TSV configuration file: {ex.Message}");
+            }
+            
+
+        }
+    }
+}
